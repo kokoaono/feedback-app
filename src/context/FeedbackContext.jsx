0ac59b888c07e4631275048fa3f633a793712e65
kokoaono <aono@Ayakos-MacBook-Pro.local>
2022-07-11 15:43:12 +1200

    add edit to context

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -6,15 +6,18 @@ const FeedbackContext = createContext();
 export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     { id: 1, text: "This item is from context", rating: 10 },
+    { id: 2, text: "item 2", rating: 5 },
   ]);
 
   const [feedbackEdit, setFeedbackEdit] = useState({ item: {}, edit: false });
+
   //Delete Feedback
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
+
   //add Feedback
   const addFeedback = (newFeedback) => {
     //create id
