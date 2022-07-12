6d5b3f7a2edb76937b9a801961404bc0bf93b2c5
kokoaono <aono@Ayakos-MacBook-Pro.local>
2022-07-12 13:31:47 +1200

    add concurrently and create db.json

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,15 +1,26 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    { id: 1, text: "This item is from context", rating: 10 },
-    { id: 2, text: "item 2", rating: 5 },
-  ]);
-
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({ item: {}, edit: false });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  //fetch feedback
+  const fetchFeedback = async () => {
+    const res = await fetch(
+      "http://localhost:5000/feedback?_sort=id&_order=desc"
+    );
+    const data = await res.json();
+    setFeedback(data);
+    setIsLoading(false);
+  };
 
   //Delete Feedback
   const deleteFeedback = (id) => {
@@ -42,6 +53,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         editFeedback,
         addFeedback,
